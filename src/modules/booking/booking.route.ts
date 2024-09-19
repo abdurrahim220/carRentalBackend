74dc7b35b8094e75a7c020c6bc963a1bb6be8552
“abdurrahim220” <z.abdurrahim5@gmail.com>
2024-09-20 00:21:06 +0600

    secure route doen

--- src/modules/booking/booking.route.ts
+++ src/modules/booking/booking.route.ts
@@ -2,15 +2,17 @@ import express from 'express';
 import validateRequest from '../../middleware/validateRequest';
 import { bookingZodValidation } from './booking.validation';
 import { bookingController } from './booking.controller';
+import auth from '../../middleware/auth';
 
 const router = express.Router();
 
 router.post(
-  '/create',
+  '/',
+  auth(['user']),
   validateRequest(bookingZodValidation.bookingValidation),
   bookingController.createBookingIntoDB,
 );
-router.get('/', bookingController.getAllBookingFromDB);
+router.get('/',auth(['admin']), bookingController.getAllBookingFromDB);
 router.get('/:id', bookingController.getSingleBookingFromDB);
 
 export const bookingsRouter = router;
